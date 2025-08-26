39396579581a9028675d0ffaef08d98b0e26e275
ashutoshsingh980756 <ashutoshsingh9807@gmail.com>
2025-08-26 12:25:37 +0530

    restructuring file

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,15 +5,13 @@ import "../../styles/globals.css";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        <main className="container mx-auto p-4">
-          <h1>Welcome to auth Section</h1>
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Header />
+      <main className="container mx-auto p-4">
+        <h1>Welcome to auth Section</h1>
+        {children}
+      </main>
+      <Footer />
+    </>
   );
 }
